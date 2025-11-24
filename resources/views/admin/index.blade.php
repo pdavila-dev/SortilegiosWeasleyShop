6ea23dcd29406d4e5e7ea873c740a6abb7f6d8d2
Paolo Davila <pdavila13@uoc.eu>
2025-11-24 07:45:52 +0100

    Cambios aplicados en la vista home y welcome

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,11 +1,26 @@
-@extends('adminlte::page')
+@extends('layouts.app')
 
-@section('title', 'Dashboard')
+{{-- Customize layout sections --}}
 
-@section('content_header')
-    <h1>Dashboard</h1>
-@stop
+@section('subtitle', 'Welcome')
+@section('content_header_title', 'Home')
+@section('content_header_subtitle', 'Welcome')
 
-@section('content')
+{{-- Content body: main page content --}}
+
+@section('content_body')
     <p>Welcome to this beautiful admin panel.</p>
 @stop
+
+{{-- Push extra CSS --}}
+
+@push('css')
+    {{-- Add here extra stylesheets --}}
+    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
+@endpush
+
+{{-- Push extra scripts --}}
+
+@push('js')
+    <script> console.log("Hi, I'm using the Laravel-AdminLTE package!"); </script>
+@endpush
